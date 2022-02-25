dd1fed769ca26449b7f858cf742a1d3a81a12ba0
duncanobrien <duncan.a.obrien@gmail.com>
2022-02-25 19:21:15 +0000

    formatted documentation

--- R/ewsnet_predict.R
+++ R/ewsnet_predict.R
@@ -5,25 +5,36 @@
 #' @param x A numeric vector of values to be tested.
 #' @param noise_type A string stating the form of noise to use. Options are "W" or "C".
 #' @param ensemble A numeric value stating the number of models to average over. Options range from 1 to 25.
-#'
+#' @param envname A string naming the Python environment prepared by \code{ewsnet_int()}.
 #' @returns A dataframe of EWSNET predictions. Values represent the estimated probability that the quoted event will occur.
 #'
 #' @examples
+#' #A dummy dataset of a hedgerow bird population
+#' #monitored over 50 years.
+#'
 #' abundance_data <- data.frame(time = seq(1:50),
 #'  abundance = rnorm(50,mean = 20))
 #'
-#' \dontrun{pred<- ewsnet_predict(
+#' #Activate python environment (only necessary
+#' #on first opening of R session).
+#'
+#' \dontrun{ewsnet_init(envname = "EWSNET_env")}
+#'
+#' #Generate EWSNET predictions.
+#'
+#' \dontrun{pred <- ewsnet_predict(
 #'  abundance_data$abundance,
 #'  noise_type = "W",
-#'  ensemble = 15)}
+#'  ensemble = 15,
+#'  envname = "EWSNET_env")}
 #'
 #' @export
 #'
 #ewsNETw_25 <- function(x, noise_type = "W", ensemble = "25"){
-ewsnet_predict <- function(x, noise_type = "W", ensemble = 25){
+ewsnet_predict <- function(x, noise_type = "W", ensemble = 25,envname){
 
-  if(length(reticulate::conda_list())<2){
-    warning("Call 'ewsnet_init()' before attempting to use ewsnet_predict()")
+  if(!envname %in% (reticulate::conda_list()$name)){
+    warning("Call 'ewsnet_init()' before attempting to use ewsnet_predict(), or check your spelling of envname")
   }else{
 
   noise_type <- match.arg(noise_type, choices = c("W","C"))
