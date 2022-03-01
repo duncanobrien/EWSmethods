3d67a4a5fe9e0ea0df8442fa17530ef1d16538e8
duncanobrien <duncan.a.obrien@gmail.com>
2022-03-01 20:41:31 +0000

    added pip_ignore_installed to ewsnet_init to speed up activation

--- R/ewsnet_init.R
+++ R/ewsnet_init.R
@@ -3,6 +3,7 @@
 #' Prepares your R session for communicating with Python. This function first searches your computer for an appropriate Python environment and activates it, importing the vital Python packages required. If no appropriate Python install or environment is found, after asking permission, Anaconda is downloaded and an environment created.
 #'
 #' @param envname A string naming the desired Python environment to create/activate. If no python or environment found, the functions prompts to install Anaconda and the required python packages.
+#' @param pip_ignore_installed Boolean. If \code{FALSE}, any packages already installed are loaded and not re-downloaded. However, if \code{TRUE}, packages are downloaded irregardless, overwriting any version already present (is analagous to updating if required).
 #'
 #' @returns Does not return an object as is simply preparing your R session.
 #'
@@ -15,7 +16,7 @@
 #'
 #' @export
 
-ewsnet_init <- function(envname){
+ewsnet_init <- function(envname,  pip_ignore_installed = FALSE){
 
   conda <- try(reticulate::conda_list()) #extract list of conda Python environments
   if(class(conda) == "try-error"){
@@ -42,7 +43,7 @@ ewsnet_init <- function(envname){
       reticulate::conda_install(envname = paste0(envname),
                                 packages = c("tensorflow","scikit-learn","pandas","matplotlib",
                                              "sphinxcontrib-matlabdomain","seaborn"),
-                                pip = T,pip_ignore_installed	=T)
+                                pip = T, pip_ignore_installed = pip_ignore_installed)
 
       message(paste(envname,"successfully installed and activated. Necessary python packages installed"),
               collapse = " ")
@@ -64,13 +65,13 @@ ewsnet_init <- function(envname){
       reticulate::conda_install(envname = paste0(envname),
                                 packages = c("tensorflow","scikit-learn","pandas","matplotlib",
                                              "sphinxcontrib-matlabdomain","seaborn"),
-                                pip = T,pip_ignore_installed	=T)
+                                pip = T, pip_ignore_installed	= pip_ignore_installed)
 
       message(paste(envname,"successfully found and activated. Necessary python packages installed"),
               collapse = " ")
     }
 
-  }else{ #if desired environment found, ask permission to create new environment
+  }else{ #if desired environment not found, ask permission to create new environment
     string <- c("conda env",envname, "not found. Would you like to install a new env? (y/n) ")
     user_input2 <- readline(paste(string,collapse =" "))
     if(user_input2 != 'y'){ message('Aborting')
@@ -87,7 +88,7 @@ ewsnet_init <- function(envname){
       reticulate::conda_install(envname = paste0(envname),
                                 packages = c("tensorflow","scikit-learn","pandas","matplotlib",
                                              "sphinxcontrib-matlabdomain","seaborn"),
-                                pip = T,pip_ignore_installed	=T)
+                                pip = T, pip_ignore_installed	= pip_ignore_installed)
 
       message(paste(envname,"successfully installed and activated. Necessary python packages installed"),
               collapse = " ")
