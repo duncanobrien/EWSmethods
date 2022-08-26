40cab309cc6d8c3cf9281b35bb9004463cd2957b
duncanobrien <duncan.a.obrien@gmail.com>
2022-08-26 11:27:00 +0100

    add Cod recovery data from Clements et al 2019

--- R/data.R
+++ R/data.R
@@ -13,4 +13,21 @@
 #'   \item{spp_5}{density of species 1}
 #'   \item{inflection_pt}{the time index where transition occurs}
 #' }
-"ewsmethods_trans_dat"
+"simTransComms"
+
+#' Three Recovering Cod Populations
+#'
+#' A dataset containing three simulated cod populations. Community 1 does not recovery whereas Community 100 and 200 do.
+#'
+#' @format A list of three dataframes with 191 rows and 6 variables each:
+#' \describe{
+#'   \item{community_id}{the identity of the simulated community}
+#'   \item{time}{time index}
+#'   \item{biomass}{population total biomass}
+#'   \item{mean.size}{average length of cod individuals}
+#'   \item{sd.size}{variation in length of cod individuals}
+#'   \item{inflection_pt}{the time index where transition occurs}
+#' }
+#' @source Clements C., McCarthy M., Blanchard J. (2019) Early warning signals of recovery in complex systems. Nature Communications, 10:1681.
+"CODrecovery"
+
