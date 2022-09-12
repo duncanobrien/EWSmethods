ab1ce3089671d4776f21425a78faca7e225bd21a
duncanobrien <duncan.a.obrien@gmail.com>
2022-09-12 17:40:11 +0100

    add self contained download to ewsnet_reset

--- R/reset_ewsnet.R
+++ R/reset_ewsnet.R
@@ -7,25 +7,38 @@
 #'
 #' @examples
 #' \dontrun{
-#' reset_ewsnet(new_weight_location = "C:/Users/your_name/Downloads/Pretrained") # Windows example
+#' ewsnet_reset(new_weight_location = "C:/Users/your_name/Downloads/Pretrained") # Windows example
 #' }
 #'
 #'\dontrun{
-#' reset_ewsnet(new_weight_location = "/Users/your_name/Downloads/Pretrained") # Mac example
+#' ewsnet_reset(new_weight_location = "/Users/your_name/Downloads/Pretrained") # Mac example
 #'}
 #'
 #' @export
 
-reset_ewsnet <- function(new_weight_location){
+ewsnet_reset <- function(new_weight_location){
 
  target_folder <- system.file("python/weights", package = "EWSmethods")
  file.copy(new_weight_location, target_folder, recursive = T)
 
- # zip <- paste(c(target_folder,"temp.zip"),collapse = "/")
- #
- # download.file("https://drive.google.com/u/0/uc?export=download&confirm=EoIm&id=1-aY2MepouLQdMSNkYD6jgSedwFXB8BUP",
- #                       destfile  = zip, overwrite = TRUE)
- # unzip(zip,exdir = target_folder)
- # file.remove(zip)
- # message("Model weights replaced")
+ message("Model weights reset")
 }
+
+
+# reset_ewsnet <- function(){
+#
+#   target_folder <- system.file("python/weights", package = "EWSmethods")
+#
+#   zip <- paste(c(target_folder,"temp.zip"),collapse = "/")
+#
+#   # download.file("https://drive.google.com/u/0/uc?export=download&confirm=EoIm&id=1-aY2MepouLQdMSNkYD6jgSedwFXB8BUP",
+#   #               destfile  = zip, overwrite = TRUE) #original EWSNet
+#   download.file("https://drive.google.com/u/0/uc?export=download&confirm=EoIm&id=19OuqzrY1LQxZusByf4ACPj-yiex4LY2e",
+#                 destfile  = zip, overwrite = TRUE) #VariableLenModel EWSNet
+#
+#   unzip(zip,exdir = target_folder)
+#   file.remove(zip)
+#
+#   message("Model weights reset")
+# }
+
