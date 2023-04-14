740d9daf2092760aa492e0257246fb02f276e637
duncanobrien <duncan.a.obrien@gmail.com>
2023-04-14 16:46:04 +0100

    attempt fix broken ewsnet_init test v2

--- tests/testthat/test-ewsnet_init.R
+++ tests/testthat/test-ewsnet_init.R
@@ -6,6 +6,6 @@ test_that("EWSNet can be correctly initialised and uninstalled", {
   reticulate::miniconda_uninstall()
   #conda <- try(reticulate::conda_list())
   #expect_true(inherits(conda, "try-error"))
-  expect_true(!("test_env" %in% reticulate::conda_list()))
+  expect_true(!("test_env" %in% reticulate::conda_list()$name))
 
 })
